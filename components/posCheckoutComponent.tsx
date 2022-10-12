179434c9d61cc668b6934a101839c8ad7198a48a
rubens <rubens.miguek@gmail.com>
2022-10-12 18:41:48 -0300

    add facebook pixel

--- components/posCheckoutComponent.tsx
+++ components/posCheckoutComponent.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Button } from "@mui/material";
 
 import Head from "next/head";
+import Script from "next/script";
 import { useRouter } from "next/router";
 
 import { useUserStore } from "../store/store";
@@ -103,6 +104,31 @@ const PosCheckoutComponent = () => {
         <meta name="description" content="Checkout" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script
+        id="facebook"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          <!-- Meta Pixel Code -->
+          <script>
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '579978647132421');
+          fbq('track', 'PageView');
+          </script>
+          <noscript><img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=579978647132421&ev=PageView&noscript=1"
+          /></noscript>
+          <!-- End Meta Pixel Code -->
+          `
+        }}
+      />
       <StatusComponent status={paymentStatus} />
     </div>
   );
